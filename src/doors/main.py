0f673690e0bfa3461f47530f4d750a163a1be710
undrwolf <74624168+undrwolf@users.noreply.github.com>
2023-12-30 00:34:15 +0100

    questions json

--- src/doors/main.py
+++ src/doors/main.py
@@ -1,20 +1,17 @@
-from tqdm import tqdm
 from rich import print as p
 from rich.table import Table
 from rich.console import Console
 from rich.panel import Panel
 from rich.text import Text
+from tqdm import tqdm
 import time
 import os
 import json
+import random
 
-multi_answer = {
-    "question": "\n# What is the output of the Python expression:\n\n>>>print(3*1**3)\n",
-    "answer_1": ["🅰", "27", "❔", "❌"],
-    "answer_2": ["🅱", "9", "❔", "❌"],
-    "answer_3": ["🅲", "3", "❔", "🔑"],
-    "explanation": "Exponents (1**3) have higher precedence than multiplication (3*1)"
-}
+path = "./src/doors/mcq.json"
+with open(path, 'r') as file:
+    question = json.load(file)
 
 correct_emoji = "🔑"
 wrong_emoji = "❌"
@@ -22,26 +19,21 @@ wrong_emoji = "❌"
 a, b, c = "🅰", "🅱", "🅲"
 question_mark = "❔"
 
-path = "./mcq.json"
-with open(path, 'r') as file:
-    question = json.load(file)
-
 console = Console()
 
 def clar_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
-
-def render_table(m_a, step):
+def render_table(m_a, answers, step):
     text = p(m_a['question'])
 
     table = Table(show_header=False)
     table.add_column(style="cyan")
     table.add_column(style="magenta")
-    table.add_row(m_a['answer_1'][0], m_a['answer_1'][1], m_a['answer_1'][step])
-    table.add_row(m_a['answer_2'][0], m_a['answer_2'][1], m_a['answer_2'][step])
-    table.add_row(m_a['answer_3'][0], m_a['answer_3'][1], m_a['answer_3'][step])
+
+    table.add_row(a, answers[0][0], answers[0][step])
+    table.add_row(b, answers[1][0], answers[1][step])
+    table.add_row(c, answers[2][0], answers[2][step])
     console.print(table, justify="center")
     if step == 3:
         time.sleep(0.1)
@@ -50,16 +42,15 @@ def render_table(m_a, step):
 
 def main(m_a):
     clar_screen()
+    answers = [[m_a['wrong_1'], question_mark, wrong_emoji], [m_a['wrong_2'], question_mark, wrong_emoji], [m_a['correct'], question_mark, correct_emoji]]
+    random.shuffle(answers)
 
-    render_table(m_a, 2)
+    render_table(m_a, answers, 1)
     print("")
 
-    for i in tqdm(range(100), bar_format='{l_bar}|{bar}||Answer '):
+    for i in tqdm(range(10), bar_format='{l_bar}|{bar}||Answer '):
     # for i in tqdm(range(10), bar_format='Answer in {remaining} | {bar}'):
         time.sleep(0.1)
 
     clar_screen()
-    render_table(m_a, 3)
-
-if __name__ == '__main__':
-    main(multi_answer)
+    render_table(m_a, answers, 2)
