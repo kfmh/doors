b444ccbfe58d1f751eaf8d5bfec7d870e9408664
undrwolf <74624168+undrwolf@users.noreply.github.com>
2024-01-20 22:25:59 +0100

    refactoring graphics.py

--- src/doors/doors_gameplay/graphics.py
+++ src/doors/doors_gameplay/graphics.py
@@ -1,28 +1,25 @@
 import curses
 import time
 from collections import Counter
+import operator
 
 class RenderASCII:
     def __init__(self, 
                  stdscr,
-                board_size_x, 
-                board_size_y,
-                path,
-                assets_xy_list,
-                doors_xy_list,
-                door_propertys
-                ):
-        self.assets_xy_list = assets_xy_list
-        self.doors_xy_list = doors_xy_list
+                 board_size_x, 
+                 board_size_y,
+                 path,
+                 assets_xy_list,
+                 doors_xy_list,
+                 door_propertys
+                 ):
+        self.stdscr = stdscr
         self.board_size_x = board_size_x
         self.board_size_y = board_size_y
-        self.stdscr = stdscr
         self.path = path
+        self.assets_xy_list = assets_xy_list
+        self.doors_xy_list = doors_xy_list
         self.door_propertys = door_propertys
-
-        # with open('render_door_graphic_ASCII.txt', 'r') as f:
-        #     door_graphics = f.readlines()
-        # self.door_graphics = [line.strip('\n') for line in door_graphics]
 
         curses.start_color()
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
@@ -36,7 +33,7 @@ class RenderASCII:
                    doors_xy_list,
                    hide_maze,
                    resource_pickups,
-                   resources,
+                   assets_graphic,
                    doors_graphic,
                    illuminate,
                    show_door,
@@ -44,91 +41,105 @@ class RenderASCII:
                    ):
         
         self.stdscr.clear()
-        r = resources
+        a = assets_graphic
         d = doors_graphic
+        resource_list = [(self.assets_xy_list, a), (self.doors_xy_list, d)]
+        vpr_list = [['.', operator.sub, 'horizontal'], 
+                    ['.', operator.add, 'horizontal'], 
+                    ['.', operator.sub, 'vertical'], 
+                    ['.', operator.add, 'vertical']] 
+        
         if show_door:
             self.ASCIIDoor(door_name, )
         else:
             for y in range(self.board_size_y):
                 for x in range(self.board_size_x):
+
                     if y == 0 or y == self.board_size_y - 1 \
                     or x == 0 or x == self.board_size_x - 1:
                         self.stdscr.addstr(y, 2 * x, '#')
+
                     elif y == player_y and x == player_x:
-                        self.stdscr.attron(curses.color_pair(2))  # Turn on red color
-                        self.stdscr.addstr(y, 2 * x, 'x')
-                        self.stdscr.attroff(curses.color_pair(2))  # Turn off red color
+                        self.curses_color_print((x, y), 2, 'x')
+
                     elif self.is_on_path(x, y):
                         if illuminate != 0:
                             hide_maze = False
+
                         if hide_maze:
-                            if (x,y) == (player_x - 1,  player_y)\
-                            or (x,y) == (player_x - 2,  player_y):
-                                self.stdscr.addstr(y, 2 * x, '.') # Path behind
+                            
+                            for i in vpr_list: # Visible_Path_Radius_list
+                                self.visible_path_radius(x, y,
+                                                        player_x, player_y,
+                                                        visual_str=i[0],
+                                                        op=i[1], direction=i[2],
+                                                        resource_list=resource_list,
+                                                        hide_maze=hide_maze
+                                                        )
                                 
-                                if (x,y) in assets_xy_list:
-                                    self.render_resources((x,y), r)
-                                elif (x,y) in doors_xy_list:
-                                    self.render_door_in_mze((x,y), d)
-
-                            elif (x,y) == (player_x + 1,  player_y)\
-                            or (x,y) == (player_x + 2,  player_y):
-
-                                self.stdscr.addstr(y, 2 * x, '.') # Path infront
-                                
-                                if (x,y) in assets_xy_list:
-                                    self.render_resources((x,y), r)
-                                elif (x,y) in doors_xy_list:
-                                    self.render_door_in_mze((x,y), d)
-
-                            elif (x,y) == (player_x,  player_y + 1) \
-                            or (x,y) == (player_x,  player_y + 2):
-                                self.stdscr.addstr(y, 2 * x, '.') # Path above
-                                
-                                if (x,y) in assets_xy_list:
-                                    self.render_resources((x,y), r)
-                                elif (x,y) in doors_xy_list:
-                                    self.render_door_in_mze((x,y), d)
-
-                            elif (x,y) == (player_x,  player_y - 1)\
-                            or (x,y) == (player_x,  player_y - 2):
-                                self.stdscr.addstr(y, 2 * x, '.') # Path below
-
-                                if (x,y) in assets_xy_list:
-                                    self.render_resources((x,y), r)
-                                elif (x,y) in doors_xy_list:
-                                    self.render_door_in_mze((x,y), d)
                         else: 
-                            if (x,y) in assets_xy_list:
-                                self.render_resources((x,y), r)
-                            elif (x,y) in doors_xy_list:
-                                self.render_door_in_mze((x,y), d)
-                            else:
-                                self.stdscr.addstr(y, 2 * x, '.')
+                            self.render_object((x,y), resource_list, hide_maze)
+                    
+            self.game_play_baar((player_x, player_y))
+                
+
+    def visible_path_radius(self, 
+                            x, y, 
+                            player_x, player_y,
+                            visual_str, op, direction, 
+                            resource_list, hide_maze):
+        """_summary_
+
+        Args:
+            visual_str (str): ASCI visual string
+            x (int): redner visual at position x
+            y (int): redner visual at position y
+            player_x (int): palyer x coordiante
+            player_y (int): player y coordinate
+
+        """
+        if direction == 'horizontal':
+            if  (x,y) == (op(player_x, 1),  player_y) or \
+                (x,y) == (op(player_x, 2),  player_y):
+                self.stdscr.addstr(y, 2 * x, visual_str) 
+                self.render_object((x, y), resource_list, hide_maze)
+                
+        else:
+            if  (x,y) == (player_x,  op(player_y, 1)) or \
+                (x,y) == (player_x,  op(player_y, 2)):
+                self.stdscr.addstr(y, 2 * x, visual_str) 
+                self.render_object((x, y), resource_list, hide_maze)
+
             
-        
-        game_play_baar = (
+
+    def game_play_baar(self, pos):
+        game_play_baar_str = (
             f'\n| {str(pos)}'
-            f' | R:{resource_pickups}'
+            # f' | R:{resource_pickups}'
         )
-        strr =Counter(self.path)
-        more_than_one = {item: strr[item] for item in strr if strr[item] > 1}
-        self.stdscr.addstr(game_play_baar)
-        self.stdscr.addstr(str(show_door))
-        self.stdscr.addstr(f'\n{str(more_than_one)}')
-        # self.stdscr.addstr(f'\n{str(assets_xy_list)}')
+        self.stdscr.addstr(game_play_baar_str)
+        # strr =Counter(self.path)
+        # more_than_one = {item: strr[item] for item in strr if strr[item] > 1}
+        # self.stdscr.addstr(f'\n{str(more_than_one)}')
 
-    def render_resources(self, crd, resources):
-        i = self.assets_xy_list.index((crd[0],crd[1]))
-        self.stdscr.attron(curses.color_pair(1))  # Turn on red color
-        self.stdscr.addstr(crd[1], 2 * crd[0], resources[i])
-        self.stdscr.attroff(curses.color_pair(1))  # Turn off red color
+    def render_object(self, coordiantes, resource_list, hide_maze):
+        if not hide_maze:
+            self.curses_color_print(coordiantes=coordiantes, 
+                                    color=2, 
+                                    graphic='.')
+            
+        for resources in resource_list:
+            if coordiantes in resources[0]:
+                i = resources[0].index(coordiantes)
+                r = resources[1][i]     
+                self.curses_color_print(coordiantes=coordiantes, 
+                                        color=1, 
+                                        graphic=r)
 
-    def render_door_in_mze(self, crd, door):
-        i = self.doors_xy_list.index((crd[0],crd[1]))
-        self.stdscr.attron(curses.color_pair(1))  # Turn on red color
-        self.stdscr.addstr(crd[1], 2 * crd[0], door[i])
-        self.stdscr.attroff(curses.color_pair(1))  # Turn off red color
+    def curses_color_print(self, coordiantes, color, graphic):
+        self.stdscr.attron(curses.color_pair(color)) 
+        self.stdscr.addstr(coordiantes[1], 2 * coordiantes[0], graphic)
+        self.stdscr.attroff(curses.color_pair(color)) 
 
 
     def is_on_path(self, x, y):
@@ -137,7 +148,7 @@ class RenderASCII:
     
 
     def ASCIIDoor(self, door_name):
-        door_property_str = "        MMMMMMMd|                          |bMMMMMMM"
+        door_property_str = "        Md|                          |bM"
         door = self.door_propertys[door_name]
         with open('render_door_graphic_ASCII.txt', 'r') as f:
             door_graphics = f.readlines()
@@ -151,7 +162,7 @@ class RenderASCII:
             pct = door[property_index]['percentage']
             ppy = door[property_index]['property']
 
-            start_property = 19 + (3 - len(str(pct)))
+            start_property = 12 + (3 - len(str(pct)))
             start_str = door_property_str[:start_property]
             
             separator_str = "% : "
@@ -168,4 +179,4 @@ class RenderASCII:
             self.stdscr.addstr(i, 0, line)
             self.stdscr.refresh()
 
-        self.stdscr.addstr(str(door))
+        # self.stdscr.addstr(str(door))
